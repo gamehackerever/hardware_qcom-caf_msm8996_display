ebae7656c45f6b18b36468236a57aa6490183248
kingsman44 <shivan.0972@gmail.com>
2021-07-26 09:39:50 +0000

    Build libcopybit without headless
    Signed-off-by: kingsman44 <shivan.0972@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,11 +2,11 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit libmemtrack hdmi_cec \
+    display-hals += libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
 endif
 
-display-hals += gralloc
+display-hals += gralloc libcopybit
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
